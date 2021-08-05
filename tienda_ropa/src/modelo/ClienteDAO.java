19aa3f1e576eada783ea7da989bcd35490624f5d
Camilo A <ing_camiloacg@hotmail.com>
2021-08-05 18:33:19 -0500

    Se agrega función de busqueda de productos por cliente

--- tienda_ropa/src/modelo/ClienteDAO.java
+++ tienda_ropa/src/modelo/ClienteDAO.java
@@ -26,4 +26,26 @@ public class ClienteDAO {
         return salida;
     }
 
+    public String listarProductosPorCliente(String idenCliente, SqliteConnection conexionBD) {
+        String salida = "";
+        try {
+            String queryProductos = "SELECT c.nombres, c.apellidos, p.nombre, p.precio " +
+                                    "FROM Clientes c JOIN Ventas v ON c.identificacion = v.idCliente " +
+                                    "JOIN VentasProductos vp ON v.codigoVenta = vp.idVenta " +
+                                    "JOIN Productos p ON vp.idProducto = p.codigoBarras " +
+                                    "WHERE c.identificacion = '" + idenCliente + "'";
+            ResultSet resultadoCliente = conexionBD.ejecutarConsulta(queryProductos);
+            while (resultadoCliente.next()) {
+                String nombreCliente = resultadoCliente.getString("nombres");
+                String apellidoCliente = resultadoCliente.getString("apellidos");
+                String nombreProducto = resultadoCliente.getString("nombre");
+                float precioProducto = resultadoCliente.getFloat("precio");
+                salida = salida.concat(nombreCliente + " - " + apellidoCliente + " - " + nombreProducto + " $" + precioProducto + "\n");
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return salida;
+    }
+
 }
--- tienda_ropa/src/vista/Menu.java
+++ tienda_ropa/src/vista/Menu.java
@@ -27,7 +27,9 @@ public class Menu {
                     String clientes = cli1.mostrarClientes(conexionBD);
                     JOptionPane.showMessageDialog(null, clientes, "Listado de clientes", JOptionPane.INFORMATION_MESSAGE);
             case "3":
-
+                    String identCliente = JOptionPane.showInputDialog(null, "Ingrese la identificación del cliente a buscar:", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
+                    String productos = cli1.listarProductosPorCliente(identCliente, conexionBD);
+                    JOptionPane.showMessageDialog(null, productos, "Listado de productos comprados por cliente", JOptionPane.INFORMATION_MESSAGE);
         }
 
     }
