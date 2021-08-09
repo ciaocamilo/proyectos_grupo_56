792f39befcbfd89e4156c33ae805b95d869ba0ca
Camilo A <ing_camiloacg@hotmail.com>
2021-08-09 18:48:49 -0500

    Se agrega controlador al MVC

--- tienda_ropa/src/vista/Menu.java
+++ tienda_ropa/src/vista/Menu.java
@@ -3,6 +3,7 @@ package vista;
 import modelo.*;
 import javax.swing.JOptionPane;
 import conexion.SqliteConnection;
+import controlador.controladorCliente;
 
 public class Menu {
 
@@ -13,7 +14,8 @@ public class Menu {
                            "3. Listar productos comprados por cliente";
         String opcionElegida = JOptionPane.showInputDialog(null, textoMenu, "Menú principal", JOptionPane.QUESTION_MESSAGE);
 
-        ClienteDAO cli1 = new ClienteDAO();
+        controladorCliente ctrlCliente = new controladorCliente();
+
         switch (opcionElegida) {
             case "1":
                     String ident = JOptionPane.showInputDialog(null, "Ingrese la identificación del cliente", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
@@ -21,15 +23,17 @@ public class Menu {
                     String apellido = JOptionPane.showInputDialog(null, "Ingrese el apellido del cliente", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
 
                     Cliente nuevoCliente = new Cliente(ident, nombre, apellido);
-
-                    cli1.insertarCliente(nuevoCliente, conexionBD);
+                    ctrlCliente.crearCliente(nuevoCliente, conexionBD);
+                    break;
             case "2":
-                    String clientes = cli1.mostrarClientes(conexionBD);
+                    String clientes = ctrlCliente.listarClientes(conexionBD);
                     JOptionPane.showMessageDialog(null, clientes, "Listado de clientes", JOptionPane.INFORMATION_MESSAGE);
+                    break;
             case "3":
                     String identCliente = JOptionPane.showInputDialog(null, "Ingrese la identificación del cliente a buscar:", "LA TIENDA DE ROPA", JOptionPane.QUESTION_MESSAGE);
-                    String productos = cli1.listarProductosPorCliente(identCliente, conexionBD);
+                    String productos = ctrlCliente.listarProductosCliente(identCliente, conexionBD);
                     JOptionPane.showMessageDialog(null, productos, "Listado de productos comprados por cliente", JOptionPane.INFORMATION_MESSAGE);
+                    break;
         }
 
     }
